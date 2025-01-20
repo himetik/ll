cc6e302f94b916632c3bc1ae1c351f47f6af5b8c
himetik <himetik@tutamail.com>
2025-01-20 23:45:14 +0300

    .

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, HTTPException
-import httpx
-from backend.models import StandardResponse, DataResponse, ErrorResponse
+from fastapi import FastAPI
+from backend.models import StandardResponse
+from backend.utils import check_url_status
 
 
 app = FastAPI()
@@ -8,26 +8,4 @@ app = FastAPI()
 
 @app.get("/check/", response_model=StandardResponse)
 async def check_server(url: str):
-    try:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(url, timeout=5)
-            if response.status_code < 400:
-                return StandardResponse(
-                    success=True,
-                    data=DataResponse(status="up", code=response.status_code)
-                )
-            else:
-                return StandardResponse(
-                    success=True,
-                    data=DataResponse(status="down", code=response.status_code)
-                )
-    except httpx.RequestError as e:
-        return StandardResponse(
-            success=False,
-            error=ErrorResponse(message="Request failed", details=str(e))
-        )
-    except Exception:
-        raise HTTPException(
-            status_code=500,
-            detail="Internal server error"
-        )
+    return check_url_status(url)
