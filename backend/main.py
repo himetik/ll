ecaf4d96ce24be2b7b08dd5b98619d68e19e10d0
himetik <himetik@tutamail.com>
2025-01-20 23:13:52 +0300

    Fix type for url

--- backend/main.py
+++ backend/main.py
@@ -9,9 +9,10 @@ app = FastAPI()
 @app.post("/check-link")
 async def check_link(link: LinkRequest):
     try:
+        url_str = str(link.url)
         async with httpx.AsyncClient() as client:
-            response = await client.get(link.url, timeout=10)
-            return {"url": link.url, "status": response.status_code}
+            response = await client.get(url_str, timeout=10)
+            return {"url": url_str, "status": response.status_code}
     except httpx.RequestError as e:
         raise HTTPException(status_code=400, detail=f"Error accessing the URL: {e}")
     except Exception as e:
